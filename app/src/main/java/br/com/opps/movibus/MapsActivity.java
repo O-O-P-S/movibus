33d6990945c1f7b30ca50a7137adc91b0b097b82
luisfchaves <contato@luischaves.com.br>
2018-01-27 23:21:40 -0300

    O Mapa não está exibindo. Alterei a cor para teste.

--- app/src/main/java/br/com/opps/movibus/MapsActivity.java
+++ app/src/main/java/br/com/opps/movibus/MapsActivity.java
@@ -2,6 +2,8 @@ package br.com.opps.movibus;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,6 +19,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
